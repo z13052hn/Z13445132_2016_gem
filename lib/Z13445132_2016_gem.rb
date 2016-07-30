f7de8cc52f428a776a391a390237ac4dd2b50e1b
None <z13052hn@aiit.ac.jp>
2016-07-30 08:35:46 +0000

    update

--- lib/Z13445132_2016_gem.rb
+++ lib/Z13445132_2016_gem.rb
@@ -3,3 +3,10 @@ require "Z13445132_2016_gem/version"
 module Z134451322016Gem
   # Your code goes here...
 end
+
+module HeikinModule
+  def heikin(x, y)
+    kekka = (x + y) / 2
+    print(kekka)
+  end
+end
