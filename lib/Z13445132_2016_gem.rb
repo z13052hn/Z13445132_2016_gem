9ce14e2905f63c9f46c0c6afdc5cc456c655fb4d
mohumohu <b1621ry@aiit.ac.jp>
2016-07-30 08:11:25 +0000

    add new module fizzbuzz

--- lib/Z13445132_2016_gem.rb
+++ lib/Z13445132_2016_gem.rb
@@ -3,3 +3,16 @@ require "Z13445132_2016_gem/version"
 module Z134451322016Gem
   # Your code goes here...
 end
+
+module Fizz_buzz
+	def judge(num)
+		if num % 15 == 0
+			puts "FizzBuzz"
+		elsif num % 5 == 0
+			puts "Buzz"
+		elsif num % 3 == 0
+			puts "Fizz"
+		end
+	end
+
+end
