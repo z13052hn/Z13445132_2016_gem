6c84d7f2815eb914785bcf8a5696552bb0bf64d2
mohumohu <b1621ry@aiit.ac.jp>
2016-07-30 08:18:46 +0000

    add new module calculator

--- lib/Z13445132_2016_gem.rb
+++ lib/Z13445132_2016_gem.rb
@@ -3,3 +3,10 @@ require "Z13445132_2016_gem/version"
 module Z134451322016Gem
   # Your code goes here...
 end
+
+module Calculator
+	def add(num1,num2)
+		result = num1 + num2
+		return result
+	end
+end
